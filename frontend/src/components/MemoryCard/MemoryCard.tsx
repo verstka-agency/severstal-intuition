277ca874d2c6c1ddb2af5ff6e15aa8098856d6d3
am-monkey <iafanasov@icloud.com>
2025-01-18 22:28:40 +0300

    feat: memory game [mocked data]

--- frontend/src/components/MemoryCard/MemoryCard.tsx
+++ frontend/src/components/MemoryCard/MemoryCard.tsx
@@ -3,35 +3,28 @@ import './MemoryCard.scss'
 import { getStyles } from "../../utils/styles"
 
 interface IMemoryCard extends React.HTMLAttributes<HTMLDivElement> {
-    isSelected: boolean
+    children: string
+    isSelected: boolean;
 }
 
 const MemoryCard: React.FC<IMemoryCard> = (props) => {
     const { children, onClick, isSelected } = props
 
-    return (
-        <div
-            className={
-                getStyles("memory-card", [
-                    {
-                        decision: isSelected,
-                        name: "flipped",
-                    }
-                ])
-            }
-            onClick={onClick}
-        >
-            <div className={`front-face`}>
-                {
-                    isSelected
-                        ?
-                        <p>{children}</p>
-                        : null
-                }
-            </div>
-            <div className={"back-face"}>
+    const currentStyle = getStyles("memory-card", [
+        {
+            decision: isSelected,
+            name: "flipped"
+        }
+    ])
 
+    return (
+        <div className={currentStyle} onClick={onClick}>
+            <div className={`front-face`}>
+                {isSelected
+                    ? <img src={children} alt="" />
+                    : null}
             </div>
+            <div className={"back-face"}></div>
         </div>
     )
 }
--- frontend/src/pages/Memory/Memory.tsx
+++ frontend/src/pages/Memory/Memory.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import MemoryCard from '../../components/MemoryCard/MemoryCard'
-import './Memory.scss'
-import { useMutation, useQuery } from "@tanstack/react-query"
-import axios from "axios"
-import { shuffleArray } from "../../utils/memoryCards"
-import ErrorMessage from "../../components/ErrorMessage/ErrorMessage"
-import Loader from '../../components/Loader/Loader'
+import React, { useEffect, useState } from 'react';
+import MemoryCard from '../../components/MemoryCard/MemoryCard';
+import './Memory.scss';
+import { useQuery } from "@tanstack/react-query";
+import { shuffleArray } from "../../utils/memoryCards";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Loader from '../../components/Loader/Loader';
 
 const Memory = () => {
-    const [selectedCards, setSelectedCards] = useState<number[]>([])
-    const [isPreviewMode, setIsPreviewMode] = useState<boolean>(true)
+    const [selectedCards, setSelectedCards] = useState<number[]>([]);
+    const [isPreviewMode, setIsPreviewMode] = useState<boolean>(true);
+    const [matchedCards, setMatchedCards] = useState<number[]>([]);
 
     // Загружаем карточки
     const { data: memoryCards, isLoading } = useQuery<string[]>({
@@ -17,87 +17,96 @@ const Memory = () => {
         queryFn: async () => {
             // Мокаем загрузку карточек
             const memoryCards = [
-                "Стабильность",
-                "Безопасность",
-                "Уважение"
+                "/memory/1.png",
+                "/memory/2.png",
+                "/memory/3.png",
+                "/memory/4.png",
             ]
 
             const doubledCards = [...memoryCards, ...memoryCards]
 
             // Функция для случайного перемешивания массива (используем алгоритм Фишера-Йетса)
-
-
             return shuffleArray(doubledCards)
         }
     })
 
-    // Добавляем баллы в случае, если ответ правильный
-    const { mutate } = useMutation({
-        mutationFn: async () => {
-            // TODO настроить эндпоинт
-            const response = await axios.post("")
-            return response.data
-        }
-    })
-
     useEffect(() => {
-        // Закрываем карточки после принятия ответов
+        if (!memoryCards) return
         if (selectedCards.length === 2) {
-            // TODO Вот тут проверяем, правильный ли ответ и вызываем mutate()
+            const [firstIndex, secondIndex] = selectedCards;
+
+            // Проверяем, совпадают ли карточки
+            if (memoryCards[firstIndex] === memoryCards[secondIndex]) {
+                // Добавляем индексы совпавших карточек в состояние matchedCards
+                setMatchedCards((prev) => [...prev, firstIndex, secondIndex]);
+            }
+
+            // Сбрасываем выбранные карточки через 1,2 секунды
             const timeout = setTimeout(() => {
-                setSelectedCards([])
-            }, 2000)
-            return () => clearTimeout(timeout)
+                setSelectedCards([]);
+            }, 1200);
+
+            return () => clearTimeout(timeout);
         }
-    }, [selectedCards])
+    }, [selectedCards, memoryCards]);
 
     useEffect(() => {
+        // Проверяем, все ли карточки перевернуты
+        if (matchedCards.length === memoryCards?.length) {
+            // TODO
+            // 1) отправка результата на бэк и увеличиваем баллы
+            // 2) переход на страницу success
+            console.log("ПОБЕДА");
+        }
+    }, [matchedCards, memoryCards]);
 
+    useEffect(() => {
         // Показываем карточки сразу после загрузки
         const timeout = setTimeout(() => {
-            setIsPreviewMode(false)
-        }, 2000)
+            setIsPreviewMode(false);
+        }, 2000);
         return () => {
-            clearTimeout(timeout)
+            clearTimeout(timeout);
+        };
+    }, [memoryCards]);
+
+    const handleCardClick = (selectedCards: number[], index: number) => {
+        if (selectedCards.length < 2 && !selectedCards.includes(index)) {
+            setSelectedCards((prev) => [...prev, index]);
         }
-    }, [memoryCards])
+    }
 
     if (!isLoading && !memoryCards) {
         return (
             <ErrorMessage />
-        )
+        );
     }
 
     return (
         <div className={"memory"}>
-            <div className={"memory__heading"}>Найди пары карточкам с ценностями</div>
+            <div className='memory__background__image memory__background__image__left'></div>
+            <div className='memory__background__image memory__background__image__right'></div>
+            <div className={"memory__heading"}>Найди пары карточкам<br /> с интересами твоих коллег</div>
             {
                 isLoading ?
                     <Loader />
                     :
                     <div className={"memory__cards"}>
-                        {memoryCards.map((card, index) => {
+                        {memoryCards && memoryCards.map((card, index) => {
                             return (
                                 <MemoryCard
                                     key={index}
-                                    onClick={() => {
-                                        if (selectedCards.length < 2 && !selectedCards.includes(index)) {
-                                            setSelectedCards((prev) => [...prev, index])
-                                        }
-                                    }}
-                                    isSelected={isPreviewMode ? true : selectedCards.includes(index)}
+                                    onClick={() => handleCardClick(selectedCards, index)}
+                                    isSelected={isPreviewMode || matchedCards.includes(index) || selectedCards.includes(index)}
                                 >
                                     {card}
                                 </MemoryCard>
-                            )
+                            );
                         })}
-                        <div>
-                            {selectedCards.length === 2 && (memoryCards[selectedCards[0]] === memoryCards[selectedCards[1]] ? "Вы получили 1 балл" : "Вы не угадали и ничего не выиграли")}
-                        </div>
                     </div>
             }
         </div>
-    )
-}
+    );
+};
 
-export default Memory
+export default Memory;
