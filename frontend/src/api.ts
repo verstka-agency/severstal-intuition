57fcb2d20a2522d178a030667d3bea74e0a176bf
lozhka13 <forsho@bk.ru>
2025-01-31 01:22:13 +0400

    fix: api calls fix

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const apiProvider = axios.create({
-    // baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
-    baseURL: `/api`,
+    baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+    // baseURL: `/api`,
 })
